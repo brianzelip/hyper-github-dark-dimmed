65a2b6955e434b3650a1ac7193c62ca6c79e7583
Brian Zelip <bzelip@protonmail.com>
2021-06-07 07:19:14 -0400

    Finalize css approch, now just need to work on the js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const gh = require('@primer/primitives/dist/json/colors/dark_dimmed.json');
-const ansi = gh.ansi;
+const bg = gh.bg;
+const ansi = gh.checks.ansi;
+const text = gh.text;
+const border = gh.border;
+const cm = gh.codemirror;
+const selected = gh.state.selected;
 
 // standard ANSI Colours
 const black = ansi.black;
@@ -23,36 +28,99 @@ const lightWhite = ansi.whiteBright;
 const primerCodemirrorBG = '#22272e';
 const primerBorderPrimary = '#444c56';
 const primerUnderlinenavBorderActive = '#f78166';
-const accentColor = blue;
+
+// vscode devtools debugging
+
+const code = {};
+code.bg = {
+  canvas: bg.canvas,
+  tabActive: '#22272e',
+  tab: ''
+};
+
+// github devtools debugging
+
+code.text = {
+  primary: text.primary,
+  secondary: text.secondary
+};
+
+code.border = {
+  primary: border.primary,
+  secondary: border.secondary
+};
+
+code.cm = {
+  text: cm.text,
+  bg: cm.bg,
+  cursor: cm.cursor
+};
+
+code.selected = {
+  primaryBg: selected.primaryBg,
+  primaryBorder: selected.primaryBorder,
+  primaryText: selected.primaryText
+};
+
+const myBG = {
+  primary: bg.canvas,
+  secondary: 'rgb(30, 34, 40)'
+  // Can't find the secondary source, but the value is all over devtools
+  // for, eg: non-active tabs, the sidebar, the terminal
+};
+const myText = {
+  primary: text.primary,
+  secondary: text.secondary
+};
 
 exports.decorateConfig = (config) =>
   Object.assign({}, config, {
-    cursorColor: accentColor,
-    cursorAccentColor: lightWhite,
-    foregroundColor: white,
-    backgroundColor: primerCodemirrorBG,
-    borderColor: primerBorderPrimary,
-    selectionColor: 'rgba(255,255,255, 0.1)',
+    cursorColor: blue,
+    foregroundColor: text.primary,
+    backgroundColor: myBG.primary,
+    borderColor: border.primary,
+    selectionColor: 'rgba(248,28,229,0.3)', //NOT WORKING ?%#$%*
     css: `
     ${config.css || ''}
+
+    .tabs_nav {
+      border-bottom-color: ${primerBorderPrimary};
+    }
+
+    .tabs_title {
+      border-top: 1px solid transparent;
+      color: ${myText.primary} !important;
+    }
+
     .tab_tab {
       border: 0;
+    }
+    .tab_tab:first-of-type {
+      padding-left: 0;
+    }
+
+    .tab_tab > .tab_text {
+      border-top: 1px solid transparent !important;
+      border-right: 1px solid ${primerBorderPrimary} !important;
       border-bottom: 1px solid ${primerBorderPrimary} !important;
-      background-color: ${primerCodemirrorBG};
+      border-left: 1px solid transparent !important;
+      background-color: ${myBG.secondary} !important;
+      color: ${myText.secondary} !important;
     }
-    .tab_text {
-      background-color: rgba(255, 255, 255, 0.02);
+    .tab_tab:first-of-type > .tab_text {
+      border-left-color: ${primerBorderPrimary} !important;
     }
-    .tab_active {
-      background-color: ${black};
+    .tab_tab:last-of-type > .tab_text {
+      border-right-color: transparent !important;
     }
-    .tab_textActive {
-      background-color: ${black};
+
+    .tab_tab > .tab_text.tab_textActive {
       border-top: 1px solid ${primerUnderlinenavBorderActive} !important;
+      border-bottom: 1px solid transparent !important;
+      color: ${myText.primary} !important;
+      background-color: ${myBG.primary} !important;
     }
-    .tabs_nav {
-      border-bottom-color: ${primerBorderPrimary} !important;
-    }
+
     .tabs_borderShim {
       display: none;
     }
